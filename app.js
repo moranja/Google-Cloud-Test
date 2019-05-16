0af3095d0644bf838f2a4419d3d697dc7af9c974
moranja <jeffadammoran@gmail.com>
2019-05-16 16:25:13 -0500

    setting up cloud

---
+++ app.js
@@ -0,0 +1,58 @@
+const pry = require('pryjs')
+const Dragon = require('./models/Dragon')
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(80);
+
+
+io.on('connection', socket => {
+    // console.log(socket)
+    socket.on('dragons.index', respond => {
+        Dragon.findAll()
+            .then( dragons => {
+                respond(dragons)
+            })
+    })
+
+    socket.on('dragons.update', async params => {
+        let dragon = await Dragon.findByPk(params.id)
+        await dragon.update(params)
+        let dragons = await Dragon.findAll()
+        io.emit('dragons.update', dragons)
+    })
+
+
+
+})
+
+
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/dragons', (req, res) => {
+    Dragon.findAll()
+        .then( dragons => {
+            res.json(dragons)
+        })
+})
+
+app.get('/dragons/:id', (req, res) => {
+    Dragon.findByPk(req.params.id)
+        .then( dragon => {
+            res.json(dragon)
+        })
+})
+
+app.patch('/dragons/:id', async (req, res) => {
+    let dragon = await Dragon.findByPk(req.params.id)
+    console.log(req.body)
+    dragon.update(req.body)
+})
+
+app.use('/',express.static('public'))
---
+++ public/precache-manifest.5a0df08a2b1cb531637c968e1603bee0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd0be88e03adffe911b25642217b367",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9229063590a17999bb3d",
+    "url": "/static/js/2.00029d87.chunk.js"
+  },
+  {
+    "revision": "486403c7a2a26804236d",
+    "url": "/static/js/main.c0207410.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
